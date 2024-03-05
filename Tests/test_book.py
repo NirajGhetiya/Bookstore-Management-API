784b5bc975e764a149f7b92db227914d1f5eaa64
MahmadaminLodhiya <mahmadamin.lodhiya@tssconsultancy.com>
2024-03-05 19:30:38 +0530

    test done

--- Tests/test_book.py
+++ Tests/test_book.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.pool import StaticPool
 
 from database import Base
-from main import app
+from app.main import app
 from Controller.bookController import get_db
 
 
@@ -31,18 +31,78 @@ app.dependency_overrides[get_db] = override_get_db
 client = TestClient(app)
 
 def test_get_books():
+    book_data = {
+        "title": "Rich dad Poor dad",
+        "isbn" : "12123321123",
+        "price": "150",
+        "author" : "Robert Kiyosaki",  
+    }
+    client.post("/book/addBooks", json=book_data)
+
     response = client.get("/book/books/")
     assert response.status_code == 200
     data = response.json()
-    assert data["data"] == []
-    assert data["status"] == "success"
+    assert data["total"] == 1
 
 def test_add_book():
     book_data = {
         "title": "Rich dad Poor dad",
-        "isbn" : "123321123",
+        "isbn" : "123321",
         "price": "150",
         "author" : "Robert Kiyosaki",  
     }
     response = client.post("/book/addBooks", json=book_data)
     assert response.status_code == 200
+    data = response.json()
+    assert data["code"] == 201
+    assert data["data"] == {
+        "price": "150",
+        "title": "Rich dad Poor dad",
+        "isbn" : "123321",
+        "author" : "Robert Kiyosaki", 
+        "id": 2
+    }
+
+def test_delete_book():
+    response = client.delete("/book/2")
+    assert response.status_code == 200
+    data = response.json()
+    assert data["msg"] == "Deleted Successfully"
+    assert data["data"] == {
+        "price": "150",
+        "title": "Rich dad Poor dad",
+        "isbn" : "123321",
+        "author" : "Robert Kiyosaki", 
+        "id": 2
+    }
+
+def test_update_book():
+    book_data = {
+        "title": "The Mastery",
+        "isbn" : "123321",
+        "price": "250",
+        "author" : "Robert Greene",  
+    }
+    response = client.put("/book/updateBook/1", json=book_data)
+    assert response.status_code == 200
+    data = response.json()
+    assert data["msg"] == "Updated Successfully"
+    assert data["data"] == {
+        "title": "The Mastery",
+        "isbn" : "123321",
+        "price": "250",
+        "author" : "Robert Greene", 
+        "id" : 1
+    }
+
+def test_search_book():
+    response = client.get("/book/search/The Mastery")
+    assert response.status_code == 200
+    data = response.json()
+    assert data["data"] == [{
+        "title": "The Mastery",
+        "isbn" : "123321",
+        "price": "250",
+        "author" : "Robert Greene", 
+        "id" : 1
+    }]
